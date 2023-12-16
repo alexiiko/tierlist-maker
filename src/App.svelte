39ca03ad0aa678798ac6367ae365e7aac1860622
alexiiko <alexiiko.msl@gmail.com>
2023-12-16 10:25:54 +0100

    added two buttons for later development. added tiers, font and stripes. changed background

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,62 @@
 <script lang="ts">
-  let count: number = 0
+  let tiers = ["S", "A", "B", "C", "D"];
+  let stripLength = tiers.length
 </script>
 
-<button on:click={() => {count += 1 }}>Inc</button>
+<main>
+  <div>
+    <button>Import files</button>
+    <button>How does it work?</button>
+  </div>
 
-<h1>{count}</h1>
+  <div class="grid">
+    <div>
+      {#each tiers as tier, _}
+        <div class="box">
+          {tier}
+        </div>
+      {/each}
+    </div>
+    <div>
+      {#each Array(stripLength) as _,i}
+        <div class="stripe">
+        </div>
+      {/each}
+    </div>
+  </div>
+  
+</main>
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100;300&display=swap" rel="stylesheet">
+
+<style>
+  main{
+    font-family: "Roboto", sans-serif;
+  }
+
+.grid {
+  display: grid;
+  grid-template-columns: 1fr 13fr;
+  grid-gap: 0px;
+}
+
+.box {
+  width: 150px;
+  height: 150px;
+  border: solid 2px black;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.stripe {
+  width: 100%;
+  height: 150px;
+  border: solid 2px black;
+  background-color: lightgrey;
+  margin: 0px;
+}
+</style>
